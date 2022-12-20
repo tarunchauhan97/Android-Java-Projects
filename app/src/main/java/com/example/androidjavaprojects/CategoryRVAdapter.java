85a66175c7ef555f8d8add8283725e137a0a403f
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-20 20:13:22 +0530

    category onBindViewHolder

--- app/src/main/java/com/example/androidjavaprojects/CategoryRVAdapter.java
+++ app/src/main/java/com/example/androidjavaprojects/CategoryRVAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -14,10 +18,12 @@ public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.Vi
 
     private ArrayList<CategoryRVModal> categoryRVModalArrayList;
     private Context context;
+    private CategoryClickInterface categoryClickInterface;
 
-    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModalArrayList, Context context) {
+    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModalArrayList, Context context, CategoryClickInterface categoryClickInterface) {
         this.categoryRVModalArrayList = categoryRVModalArrayList;
         this.context = context;
+        this.categoryClickInterface = categoryClickInterface;
     }
 
     @NonNull
@@ -31,6 +37,22 @@ public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.Vi
     @Override
     public void onBindViewHolder(@NonNull CategoryRVAdapter.ViewHolder holder, int position) {
 
+        CategoryRVModal categoryRVModal = categoryRVModalArrayList.get(holder.getAdapterPosition());
+        holder.categoryTV.setText(categoryRVModal.getCategory());
+        Picasso.get().load(categoryRVModal.getCategoryImageUrl()).into(holder.categoryIV);
+////////////////////////////////////
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                categoryClickInterface.onCategoryClick(holder.getAdapterPosition());
+            }
+        });
+
+    }
+
+    public interface CategoryClickInterface {
+        void onCategoryClick(int position);
+
     }
 
     @Override
@@ -39,8 +61,17 @@ public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.Vi
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView categoryTV;
+        private ImageView categoryIV;
+
         public ViewHolder(@NonNull View itemView) {
+
+
             super(itemView);
+            categoryIV = itemView.findViewById(R.id.idIVCategory);
+            categoryTV = itemView.findViewById(R.id.idTVCategories);
+
+
         }
     }
 }
