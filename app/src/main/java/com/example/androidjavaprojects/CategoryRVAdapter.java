035a558d54ee47ab1d5da7245789caaf754bf381
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-20 14:10:33 +0530

    news category adapter and detail activity getextra

--- app/src/main/java/com/example/androidjavaprojects/CategoryRVAdapter.java
+++ app/src/main/java/com/example/androidjavaprojects/CategoryRVAdapter.java
@@ -1,9 +1,46 @@
 package com.example.androidjavaprojects;
 
-public class CategoryRVAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 
+public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.ViewHolder> {
 
+    private ArrayList<CategoryRVModal> categoryRVModalArrayList;
+    private Context context;
 
+    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModalArrayList, Context context) {
+        this.categoryRVModalArrayList = categoryRVModalArrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CategoryRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.categories_rv_item, parent, false);
+        return new CategoryRVAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryRVAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryRVModalArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/androidjavaprojects/NewsDetailActivity.java
+++ app/src/main/java/com/example/androidjavaprojects/NewsDetailActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
+    String title, desc, content, imageURL, url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+        title = getIntent().getStringExtra("title");
+        desc = getIntent().getStringExtra("desc");
+        content = getIntent().getStringExtra("content");
+        imageURL = getIntent().getStringExtra("image");
+        url = getIntent().getStringExtra("url");
+
     }
-
-
-
 
 }
