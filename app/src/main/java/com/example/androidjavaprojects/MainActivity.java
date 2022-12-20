1234ea7352008ebde8242e68b9d084449b92d35e
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-20 12:43:10 +0530

    modal classes and retro API

--- app/src/main/java/com/example/androidjavaprojects/MainActivity.java
+++ app/src/main/java/com/example/androidjavaprojects/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //f9a1445c523b4aa88f383f6dc0fc408e
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
