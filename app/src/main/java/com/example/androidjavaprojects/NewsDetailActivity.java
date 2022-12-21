344595ec19b918ce816b52846ff79730b68d87a5
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-21 12:02:36 +0530

    news app final done

--- app/src/main/java/com/example/androidjavaprojects/NewsDetailActivity.java
+++ app/src/main/java/com/example/androidjavaprojects/NewsDetailActivity.java
@@ -2,11 +2,22 @@ package com.example.androidjavaprojects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
     String title, desc, content, imageURL, url;
+    private TextView titleTV, subDescTV, contentTV;
+    private ImageView imageNewsView;
+    private Button readNewsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,23 @@ public class NewsDetailActivity extends AppCompatActivity {
         content = getIntent().getStringExtra("content");
         imageURL = getIntent().getStringExtra("image");
         url = getIntent().getStringExtra("url");
+        titleTV = findViewById(R.id.idTVTitle);
+        subDescTV = findViewById(R.id.idTVSubDesc);
+        contentTV = findViewById(R.id.idTVContent);
+        imageNewsView = findViewById(R.id.idIVNews);
+        readNewsBtn = findViewById(R.id.idBtnReadNews);
+        titleTV.setText(title);
+        subDescTV.setText(desc);
+        contentTV.setText(content);
+        Picasso.get().load(imageURL).into(imageNewsView);
+        readNewsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+//                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
     }
 
