4d865e250ece303bbce194cb439330c065092957
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-26 12:19:37 +0530

    mvvm login xml UI done final working

--- app/src/main/java/com/example/androidjavaprojects/MainActivity.java
+++ app/src/main/java/com/example/androidjavaprojects/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -18,12 +19,13 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvDrinkName;
+    TextView tvDrinkName, tvLoginResult;
     ProgressBar progressBar;
     Button bGetDrink;
 
     MainViewModel mViewModel;
-
+    EditText etEmail, etPassword;
+    Button bLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
         tvDrinkName = findViewById(R.id.tvDrinkName);
         progressBar = findViewById(R.id.progressBar);
         bGetDrink = findViewById(R.id.bGetDrink);
-
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.bLogin);
+        tvLoginResult = findViewById(R.id.tvLoginResult);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
         mViewModel.getProgress().observe(this, new Observer<Integer>() {
@@ -55,6 +60,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mViewModel.suggestNewDrink();
+            }
+        });
+        mViewModel.getLoginResult().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tvLoginResult.setText(s);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.login(etEmail.getText().toString(), etPassword.getText().toString());
             }
         });
     }
--- app/src/main/java/com/example/androidjavaprojects/MainViewModel.java
+++ app/src/main/java/com/example/androidjavaprojects/MainViewModel.java
@@ -1,5 +1,6 @@
 package com.example.androidjavaprojects;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -13,13 +14,41 @@ public class MainViewModel extends ViewModel {
 
     MutableLiveData<Integer> mProgressMutableData = new MutableLiveData<>();
     MutableLiveData<String> mDrinksMutableData = new MutableLiveData<>();
+    MutableLiveData<String> mLoginResultMutableData = new MutableLiveData<>();
     MainRepository mMainRepository;
+
 
     public MainViewModel() {
         mProgressMutableData.postValue(View.INVISIBLE);
         mDrinksMutableData.postValue("");
+        mLoginResultMutableData.postValue("Not logged in");
         mMainRepository = new MainRepository();
     }
+
+    public void login(String email, String password) {
+        mProgressMutableData.postValue(View.VISIBLE);
+        mLoginResultMutableData.postValue("Checking");
+        mMainRepository.loginRemote(new LoginBody(email, password), new MainRepository.ILoginResponse() {
+            @Override
+            public void onResponse(LoginResponse loginResponse) {
+                Log.i("myloginapi", "---tar---"+loginResponse.getToken());
+                mProgressMutableData.postValue(View.INVISIBLE);
+                mLoginResultMutableData.postValue("Login Success");
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                mProgressMutableData.postValue(View.INVISIBLE);
+                Log.i("myloginapi", "----tar--"+t.getLocalizedMessage());
+                Log.i("myloginapi", "---tar---"+t.getMessage());
+                Log.i("myloginapi", "----tar--"+t.toString());
+
+
+                mLoginResultMutableData.postValue("Login failure: " + t.getLocalizedMessage() + "-");
+            }
+        });
+    }
+
 
     public void suggestNewDrink() {
         mProgressMutableData.postValue(View.VISIBLE);
@@ -36,7 +65,10 @@ public class MainViewModel extends ViewModel {
                 //show toast
             }
         });
+    }
 
+    public LiveData<String> getLoginResult() {
+        return mLoginResultMutableData;
     }
 
     public LiveData<Integer> getProgress() {
