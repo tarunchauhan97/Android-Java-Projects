bce920da86a6de78e036e57cda8eaf04635bbf61
Tarun Singh chauhan <chauhan.tarun897@gmail.com>
2022-12-26 11:14:29 +0530

    login method completed part three

--- app/src/main/java/com/example/androidjavaprojects/MainRepository.java
+++ app/src/main/java/com/example/androidjavaprojects/MainRepository.java
@@ -6,6 +6,10 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainRepository {
 
     String[] drinksListRemote = {"Mint Margarita", "Spiking coffee", "Sweet Bananas",
@@ -16,6 +20,41 @@ public class MainRepository {
     public MainRepository() {
 
     }
+
+    public void loginRemote(LoginBody loginBody, ILoginResponse loginResponse) {
+
+        ILoginService loginService = RetrofitClientInstance.getInstance().create(ILoginService.class);
+        Call<LoginResponse> initiateLogin = loginService.login(loginBody);
+
+        initiateLogin.enqueue(
+
+                new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                        if (response.isSuccessful()) {
+                            loginResponse.onResponse(response.body());
+                        } else {
+                            loginResponse.onFailure(new Throwable(response.message()));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        loginResponse.onFailure(t);
+                    }
+                }
+        );
+
+    }
+
+    public interface ILoginResponse {
+
+        void onResponse(LoginResponse loginResponse);
+
+        void onFailure(Throwable t);
+
+    }
+
 
     public void suggestNewDrink(IDrinkCallback drinkCallback) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -34,8 +73,7 @@ public class MainRepository {
                 } catch (InterruptedException e) {
                     drinkCallback.onErrorOccurred();
                     e.printStackTrace();
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     drinkCallback.onErrorOccurred();
                     e.printStackTrace();
                 }
